e938bf723e665c421b2709bcd177e4cd66bdc7f3
ZihanWang314 <510642032wzh@gmail.com>
2024-10-18 20:06:02 +0000

    udpate serving

--- serve.sh
+++ serve.sh
@@ -1,5 +1,21 @@
-CONDA_PREFIX=$HOME/miniconda3
-source ${CONDA_PREFIX}/etc/profile.d/conda.sh # initialize conda for non-interactive shell
+# if currently have any services, stop them
+for port in $(seq 36001 36005); do
+    # Check if there's a process using the port
+    pid=$(lsof -t -i:$port)
+    
+    if [ -n "$pid" ]; then
+        echo "Stopping process $pid using port $port..."
+        # Kill the process using the port
+        kill -9 $pid
+        echo "Process on port $port has been stopped."
+    else
+        echo "No process found on port $port."
+    fi
+done
+
+# Start the services
+CONDA_HOME=/opt/conda
+source ${CONDA_HOME}/etc/profile.d/conda.sh # initialize conda for non-interactive shell
 
 conda activate agentenv-alfworld
 pushd agentenv-alfworld
@@ -28,3 +44,12 @@ popd
 
 # conda activate agentenv-webshop
 # webshop --host 0.0.0.0 --port 36006
+
+echo "All services have been started. Testing..."
+sleep 5
+curl http://localhost:36001
+curl http://localhost:36002
+curl http://localhost:36003
+curl http://localhost:36004
+curl http://localhost:36005
+echo "All services tested"
